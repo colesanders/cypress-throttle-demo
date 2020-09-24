2ce284d01ef1fe5972883e6aef851143a1b58823
Cole Sanders <cole.sanders@briebug.com>
2020-09-24 16:19:59 -0600

    minor updates

--- cypress/integration/throttle.spec.ts
+++ cypress/integration/throttle.spec.ts
@@ -7,36 +7,54 @@ import {
   checkIfDone,
 } from 'cypress/support/click-test.po';
 
-describe('Throttle Test, Adds 10 Filler Pages for Weight', () => {
+describe('Throttle Test With Filler Pages for Weight', () => {
   beforeEach(() => {
     cy.safeVisit('/');
 
     toggleMenu();
     getMenuItem(2).wait(500).ionClick();
     getPageData().should('be.visible');
-
-    addFillerPages(10);
-
-    navToThrottle();
   });
 
-  it(`Standard Timeout`, () => {
-    checkIfDone();
+  describe('10 Filler Pages', () => {
+    beforeEach(() => {
+      addFillerPages(10);
+      navToThrottle();
+    });
+
+    it.only(`Standard Timeout`, () => {
+      checkIfDone();
+    });
+
+    it(`Timeout for 6 seconds`, () => {
+      checkIfDone(6000);
+    });
+
+    it(`Timeout for 8 seconds`, () => {
+      checkIfDone(8000);
+    });
+
+    it(`Timeout for 10 seconds`, () => {
+      checkIfDone(10000);
+    });
   });
 
-  it(`Timeout for 6 seconds`, () => {
-    checkIfDone(6000);
-  });
+  describe('20 Filler Pages', () => {
+    beforeEach(() => {
+      addFillerPages(20);
+      navToThrottle();
+    });
 
-  it(`Timeout for 8 seconds`, () => {
-    checkIfDone(8000);
-  });
+    it(`Standard Timeout`, () => {
+      checkIfDone();
+    });
 
-  it(`Timeout for 10 seconds`, () => {
-    checkIfDone(10000);
-  });
+    it(`Timeout for 10 seconds`, () => {
+      checkIfDone(20000);
+    });
 
-  it.only(`Timeout for 12 seconds`, () => {
-    checkIfDone(12000);
+    it(`Timeout for 20 seconds`, () => {
+      checkIfDone(20000);
+    });
   });
 });
